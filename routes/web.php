8ee453b4128b8992153cfa319531ccd400ac9a98
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-11-04 17:12:43 +0000

    Aggiunto esercizio

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        "nome" => "Marco",
+        "cognome" => "Porcaro",
+    ];
+    return view('home', $data);
 });
